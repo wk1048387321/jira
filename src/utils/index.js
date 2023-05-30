52f6136b893d837fb96e999a7e8309a237cebb66
Alex Wang <1048387321@qq.com>
2023-05-30 14:57:10 +0800

    fetch: 添加工程列表 - 关闭调试打印

--- src/utils/index.js
+++ src/utils/index.js
@@ -5,11 +5,10 @@ export const cleanObject = (obj) => {
 
     Object.keys(res).forEach(key => {
         const value = res[key];
-        console.log(key, value, isFalsy(value))
+
         if(isFalsy(value)) {
             delete res[key];
         }
-        console.log(res)
     })
 
     return res
