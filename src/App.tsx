6332ad6de2395a4d97c439d730f827e3a94f4d9f
Alex Wang <1048387321@qq.com>
2023-06-01 17:14:09 +0800

    fetch: json-server 配置middleware

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import {ProjectListScreen} from "./screens/project-list";
 import {TsReactTest} from "./screens/try-use-array";
+import {LoginScreen} from "./screens/login";
 
 function App() {
   return (
     <div className="App">
 
-      <ProjectListScreen />
+      {/*<ProjectListScreen />*/}
 
-      <TsReactTest />
+      {/*<TsReactTest />*/}
+
+
+        <LoginScreen />
 
     </div>
   );
